afe1b2015c3eeb82cf3b3ad8efe405e8228c8006
akaruchy <katia9@bk.ru>
2022-11-22 01:05:09 +0300

    l-4

---
+++ 1_Les/5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача №4. Напишите программу вычисления модуля числа.
+//6 -> 6
+//-82 -> 82
+
+Console.Write("Введите число:");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
